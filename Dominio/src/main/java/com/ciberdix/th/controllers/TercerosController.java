7498529fd868c2049547120a1b243a6cb1e94cfe
jjbrival <johnbriceno@ciberdix.com>
2017-03-08 15:05:41 -0500

    Controladores y Repositorios : Terceros , hubo errror de sincronizacion de archivos

---
+++ Dominio/src/main/java/com/ciberdix/th/controllers/TercerosController.java
@@ -0,0 +1,38 @@
+package com.ciberdix.th.controllers;
+
+import com.ciberdix.th.models.TercerosEntity;
+import com.ciberdix.th.repositories.TercerosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Transactional
+@RequestMapping("/employees")
+public class TercerosController {
+
+    @Autowired
+    private TercerosRepository tercerosRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<TercerosEntity> getLists() {
+        return (List<TercerosEntity>) tercerosRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{idLista}")
+    TercerosEntity findList(@PathVariable Integer idLista) {
+        return tercerosRepository.findOne(idLista);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    TercerosEntity createList(@RequestBody TercerosEntity c) {
+        return tercerosRepository.save(new TercerosEntity(c.getIdTercero(),c.getPrimerNombre(),c.getSegundoNombre(),c.getPrimerApellido(),c.getSegundoApellido(),c.getImagen(),c.getIdTipoDocumento(),c.getNumeroDocumento(),c.getFechaDocumento(),c.getIdCiudadExpDocumento(),c.getFechaNacimiento(),c.getIdCiudadNacimiento(),c.getIdGenero(),c.getIdEstadoCivil(),c.getIdFactorRh(),c.getFactorRh(),c.getNroHijos(),c.getIdLateralidad(),c.getIdTipoVivienda(),c.getEstrato(),c.getTallaCamisa(),c.getTallaPantalon(),c.getTallaCalzado(),c.getCorreoElectronico(),c.getIdLocalizacion(),c.getFechaCreacion(),c.getIdTipoPersona(),c.getRazonSocial(),c.getTalla(),c.getPeso(),c.getImc(),c.getIdProfesion(),c.getIdNivelEducacion(),c.getIdVehiculo(),c.getAuditoriaUsuario(),c.getAuditoriaFecha()));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    TercerosEntity updateList(@RequestBody TercerosEntity centrosCostosEntity) {
+        return tercerosRepository.save(centrosCostosEntity);
+    }
+}
---
+++ Dominio/src/main/java/com/ciberdix/th/repositories/TercerosRepository.java
@@ -0,0 +1,10 @@
+package com.ciberdix.th.repositories;
+
+import com.ciberdix.th.models.TercerosEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by felip on 2/03/2017.
+ */
+public interface TercerosRepository extends CrudRepository<TercerosEntity, Integer> {
+}
