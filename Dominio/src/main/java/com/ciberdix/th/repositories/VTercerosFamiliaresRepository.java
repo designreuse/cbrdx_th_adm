b455b4a55f580508fc0fce282bad7220bce91aa1
vladeye <vladimirodriguez@gmail.com>
2017-03-12 12:49:55 -0500

    Implementation to VTercerosFamiliares

---
+++ Dominio/src/main/java/com/ciberdix/th/repositories/VTercerosFamiliaresRepository.java
@@ -0,0 +1,13 @@
+package com.ciberdix.th.repositories;
+
+import com.ciberdix.th.models.VTercerosFamiliaresEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by felip on 2/03/2017.
+ */
+public interface VTercerosFamiliaresRepository extends CrudRepository<VTercerosFamiliaresEntity, Integer> {
+    List<VTercerosFamiliaresEntity> findByIdTerceros(int idTerceros);
+}
