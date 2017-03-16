9dbe0e393a9d6567bb7fed40c8b8078bc29a35e1
robertochajin <roberto.chajin@ciberdix.com>
2017-03-15 19:04:42 -0500

    Actualizacion findByIdFamiliarAndIndicadorHabilitado

--- Dominio/src/main/java/com/ciberdix/th/controllers/VTercerosFamiliaresController.java
+++ Dominio/src/main/java/com/ciberdix/th/controllers/VTercerosFamiliaresController.java
@@ -41,7 +41,7 @@ public class VTercerosFamiliaresController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/habilitated/{employee}")
     List<VTercerosFamiliaresEntity> getListsEmployeeHabilitated(@PathVariable Long employee) {
-        return   tercerosFamiliaresRepository.findByIdTerceroAndIndicadorHabilitado(employee, true);
+        return   tercerosFamiliaresRepository.findByIdFamiliarAndIndicadorHabilitado(employee, true);
 
     }
 
