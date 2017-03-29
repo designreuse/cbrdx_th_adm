e80708f2ea09913b14770128fae5e5817637f140
robertochajin <roberto.chajin@ciberdix.com>
2017-03-29 11:52:51 -0500

    Actualización del tercero en el POST

--- Dominio/src/main/java/com/ciberdix/th/controllers/refactor/TercerosRefactorController.java
+++ Dominio/src/main/java/com/ciberdix/th/controllers/refactor/TercerosRefactorController.java
@@ -36,12 +36,22 @@ public class TercerosRefactorController {
     }
     
     @RequestMapping(method = RequestMethod.POST)
-    Terceros crearTercero(@RequestBody Terceros tercero){
-        return tercerosRepository.save(tercero);
+    Terceros crearTercero(@RequestBody Terceros obj){
+        return tercerosRepository.save(new Terceros(obj.getPrimerNombre(), obj.getSegundoNombre(), 
+                obj.getPrimerApellido(), obj.getSegundoApellido(), obj.getImagen(), obj.getIdTipoDocumento(), obj.getNumeroDocumento(), 
+                obj.getFechaDocumento(), obj.getIdCiudadExpDocumento(), obj.getFechaNacimiento(), obj.getIdCiudadNacimiento(), 
+                obj.getIdGenero(), obj.getIdEstadoCivil(), obj.getIdFactorRh(), obj.getNroHijos(), 
+                obj.getIdLateralidad(), obj.getIdTipoVivienda(), obj.getEstrato(), obj.getTallaCamisa(), 
+                obj.getTallaPantalon(), obj.getTallaCalzado(), obj.getCorreoElectronico(), obj.getFechaCreacion(), 
+                obj.getIdTipoPersona(), obj.getRazonSocial(), obj.getTalla(), obj.getPeso(), obj.getImc(), 
+                obj.getIdProfesion(), obj.getIdNivelEducacion(), obj.getIdVehiculo(), obj.getAuditoriaUsuario(), 
+                obj.getAuditoriaFecha(), obj.getTelefonoFijo(), obj.getTelefonoCelular(), obj.getFechaDefuncion(), 
+                obj.getIdTipoOcupacion(), obj.getIdSectorEconomico(), obj.getIdActividadEconomica(), obj.getIdOcupacion(), 
+                obj.getIdEstadoJuridico(), obj.getIdCoberturaSalud(), obj.getIndicadorVivo(), obj.getIdTipoAfiliacion()));
     }
     
     @RequestMapping(method = RequestMethod.PUT)
-    void actualizarTercero(@RequestBody Terceros tercero){
-        tercerosRepository.save(tercero);
+    void actualizarTercero(@RequestBody Terceros obj){
+        tercerosRepository.save(obj);
     }
 }
