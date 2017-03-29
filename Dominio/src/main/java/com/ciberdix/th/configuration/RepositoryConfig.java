3446750ca26e7e74049c8e6e04361002989008fe
robertochajin <roberto.chajin@ciberdix.com>
2017-03-29 14:51:37 -0500

    VTerceros y Ocupaciones

--- Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
+++ Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
@@ -59,13 +59,10 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
         config.exposeIdsFor(ListasEstadosJuridicos.class);
         config.exposeIdsFor(ListasTiposOcupaciones.class);
         config.exposeIdsFor(ListasCoberturasSalud.class);
-<<<<<<< HEAD
         config.exposeIdsFor(VTerceros.class);
-=======
         config.exposeIdsFor(VTercerosEstudiosFormales.class);
         config.exposeIdsFor(VTercerosEstudiosNoFormales.class);
         config.exposeIdsFor(TercerosEstudiosNoFormales.class);
->>>>>>> master
 
     }
 }
