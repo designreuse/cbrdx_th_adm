e7d077e6653649cb4d18ebc9a52086bda8c4f005
robertochajin <roberto.chajin@ciberdix.com>
2017-03-29 10:17:32 -0500

    Servicios de listas de estados jurídicos, tipos de ocupaciones y coberturas salud

---
+++ Dominio/src/main/java/com/ciberdix/th/controllers/refactor/ListasTiposOcupacionesRefactorController.java
@@ -0,0 +1,47 @@
+package com.ciberdix.th.controllers.refactor;
+
+import com.ciberdix.th.models.refactor.ListasTiposOcupaciones;
+import com.ciberdix.th.repositories.refactor.ListasTiposOcupacionesRefactorRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Roberto Chajin Ortiz
+ */
+@RestController
+@Transactional
+@RequestMapping("/api/tiposOcupaciones")
+@CrossOrigin
+public class ListasTiposOcupacionesRefactorController {
+
+    @Autowired
+    private ListasTiposOcupacionesRefactorRepository tiposOcupacionesRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<ListasTiposOcupaciones> listarTiposOcupaciones() {
+        return (List<ListasTiposOcupaciones>) tiposOcupacionesRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    ListasTiposOcupaciones obtenerTiposOcupaciones(@PathVariable Integer id) {
+        return tiposOcupacionesRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    ListasTiposOcupaciones crearListasTiposOcupaciones(@RequestBody ListasTiposOcupaciones obj) {
+        return tiposOcupacionesRepository.save(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    void actualizarListasTiposOcupaciones(@RequestBody ListasTiposOcupaciones obj) {
+        tiposOcupacionesRepository.save(obj);
+    }
+}
---
+++ Dominio/src/main/java/com/ciberdix/th/repositories/refactor/ListasCoberturasSaludRefactorRepository.java
@@ -0,0 +1,12 @@
+package com.ciberdix.th.repositories.refactor;
+
+import com.ciberdix.th.models.refactor.ListasCoberturasSalud;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Roberto Chajin Ortiz
+ */
+public interface ListasCoberturasSaludRefactorRepository extends CrudRepository<ListasCoberturasSalud, Integer>{
+    
+}
