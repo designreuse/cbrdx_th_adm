103706476ac0b144186bfecdd2e1ed6531a79615
robertochajin <roberto.chajin@ciberdix.com>
2017-04-03 16:22:14 -0500

    Servicio de ListasTallas

--- Dominio/src/main/java/com/ciberdix/th/models/refactor/ListasTallas.java
+++ Dominio/src/main/java/com/ciberdix/th/models/refactor/ListasTallas.java
@@ -5,11 +5,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Timestamp;
+import javax.persistence.Table;
 
 /**
  * Created by Felipe Alejandro Aguirre Santos on 4/3/2017.
  */
 @Entity
+@Table(name = "ListasTallas", schema = "crz_th", catalog = "CREZCAMOS")
 public class ListasTallas {
     private Integer idListaTalla;
     private String codigo;
