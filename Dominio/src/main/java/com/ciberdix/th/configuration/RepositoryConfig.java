58b7ea7b795d7a4d918341b3d54ba9a7a4a9a3ba
robertochajin <roberto.chajin@ciberdix.com>
2017-04-03 11:06:47 -0500

    Servicio de ListasLateralidades

--- Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
+++ Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
@@ -76,5 +76,7 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
         config.exposeIdsFor(VTercerosFamiliares.class);
         config.exposeIdsFor(Constantes.class);
         config.exposeIdsFor(ListasTiposDatos.class);
+        config.exposeIdsFor(ListasLateralidades.class);
+        
     }
 }
