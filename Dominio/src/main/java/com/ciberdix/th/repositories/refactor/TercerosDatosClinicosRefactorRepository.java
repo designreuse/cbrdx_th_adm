e6e01bbbd7a606b8f1b4c1b851e744e9b31c9b85
robertochajin <roberto.chajin@ciberdix.com>
2017-04-04 11:47:57 -0500

    Servicio de TercerosDatosClinicos

---
+++ Dominio/src/main/java/com/ciberdix/th/repositories/refactor/TercerosDatosClinicosRefactorRepository.java
@@ -0,0 +1,11 @@
+package com.ciberdix.th.repositories.refactor;
+
+import com.ciberdix.th.models.refactor.TercerosDatosClinicos;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Roberto Chajin Ortiz
+ */
+public interface TercerosDatosClinicosRefactorRepository extends CrudRepository<TercerosDatosClinicos, Long> {
+}
