a8935f9721ea761f24e121da006acd01193a0c34
hackup86 <felipe.aguirre86@hotmail.com>
2017-04-07 10:32:29 -0500

    Repositorio Productividades
    Productividades

---
+++ Dominio/src/main/java/com/ciberdix/th/repositories/refactor/ProductividadesRefactorRepository.java
@@ -0,0 +1,10 @@
+package com.ciberdix.th.repositories.refactor;
+
+import com.ciberdix.th.models.refactor.Productividades;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Felipe Alejandro Aguirre Santos on 4/3/2017.
+ */
+public interface ProductividadesRefactorRepository extends CrudRepository<Productividades, Integer> {
+}
