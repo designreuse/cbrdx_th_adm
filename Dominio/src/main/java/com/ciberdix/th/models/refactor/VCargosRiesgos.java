90ecffad2b2a47eab554334302560ea78d6ab55e
hackup86 <felipe.aguirre86@hotmail.com>
2017-04-08 09:43:52 -0500

    Actualizacion de Objetos
    Creacion de Ovjetos de Dominio

--- Dominio/src/main/java/com/ciberdix/th/models/refactor/VCargosRiesgos.java
+++ Dominio/src/main/java/com/ciberdix/th/models/refactor/VCargosRiesgos.java
@@ -1,19 +1,26 @@
 package com.ciberdix.th.models.refactor;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
+/**
+ * Created by Felipe Alejandro Aguirre Santos on 4/8/2017.
+ */
 @Entity
-@Table(name = "CargosRiesgos", schema = "crz_th", catalog = "CREZCAMOS")
-public class CargosRiesgos {
+@Table(name = "V_CargosRiesgos", schema = "crz_th", catalog = "CREZCAMOS")
+public class VCargosRiesgos {
     private Integer idCargoRiesgo;
     private Integer idCargo;
+    private String cargo;
     private Integer idRiesgo;
+    private String riesgo;
     private Integer auditoriaUsuario;
     private Timestamp auditoriaFecha;
 
-    @Id
-    @GeneratedValue
+    @Basic
     @Column(name = "IdCargoRiesgo")
     public Integer getIdCargoRiesgo() {
         return idCargoRiesgo;
@@ -34,6 +41,16 @@ public class CargosRiesgos {
     }
 
     @Basic
+    @Column(name = "Cargo")
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Basic
     @Column(name = "IdRiesgo")
     public Integer getIdRiesgo() {
         return idRiesgo;
@@ -41,6 +58,16 @@ public class CargosRiesgos {
 
     public void setIdRiesgo(Integer idRiesgo) {
         this.idRiesgo = idRiesgo;
+    }
+
+    @Basic
+    @Column(name = "Riesgo")
+    public String getRiesgo() {
+        return riesgo;
+    }
+
+    public void setRiesgo(String riesgo) {
+        this.riesgo = riesgo;
     }
 
     @Basic
@@ -68,12 +95,14 @@ public class CargosRiesgos {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        CargosRiesgos that = (CargosRiesgos) o;
+        VCargosRiesgos that = (VCargosRiesgos) o;
 
         if (idCargoRiesgo != null ? !idCargoRiesgo.equals(that.idCargoRiesgo) : that.idCargoRiesgo != null)
             return false;
         if (idCargo != null ? !idCargo.equals(that.idCargo) : that.idCargo != null) return false;
+        if (cargo != null ? !cargo.equals(that.cargo) : that.cargo != null) return false;
         if (idRiesgo != null ? !idRiesgo.equals(that.idRiesgo) : that.idRiesgo != null) return false;
+        if (riesgo != null ? !riesgo.equals(that.riesgo) : that.riesgo != null) return false;
         if (auditoriaUsuario != null ? !auditoriaUsuario.equals(that.auditoriaUsuario) : that.auditoriaUsuario != null)
             return false;
         if (auditoriaFecha != null ? !auditoriaFecha.equals(that.auditoriaFecha) : that.auditoriaFecha != null)
@@ -86,7 +115,9 @@ public class CargosRiesgos {
     public int hashCode() {
         int result = idCargoRiesgo != null ? idCargoRiesgo.hashCode() : 0;
         result = 31 * result + (idCargo != null ? idCargo.hashCode() : 0);
+        result = 31 * result + (cargo != null ? cargo.hashCode() : 0);
         result = 31 * result + (idRiesgo != null ? idRiesgo.hashCode() : 0);
+        result = 31 * result + (riesgo != null ? riesgo.hashCode() : 0);
         result = 31 * result + (auditoriaUsuario != null ? auditoriaUsuario.hashCode() : 0);
         result = 31 * result + (auditoriaFecha != null ? auditoriaFecha.hashCode() : 0);
         return result;
