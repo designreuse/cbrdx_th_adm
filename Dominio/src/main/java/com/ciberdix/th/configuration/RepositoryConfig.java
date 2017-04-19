3f0973f5d2c053b7f9d9fca7cdd11f1a777c685b
hackup86 <felipe.aguirre86@hotmail.com>
2017-04-19 16:31:31 -0500

    Correccion
    Campos Faltantes de

--- Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
+++ Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
@@ -159,5 +159,6 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
         config.exposeIdsFor(VProyeccionLaboral.class);
         config.exposeIdsFor(Menus.class);
         config.exposeIdsFor(Roles.class);
+        config.exposeIdsFor(ActividadesEconomicasTipos.class);
     }
 }
