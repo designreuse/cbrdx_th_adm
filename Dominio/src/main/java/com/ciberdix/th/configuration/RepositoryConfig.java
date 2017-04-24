a040fcea3e94537a04b9d8aa8c8b4cb978339279
robertochajin <roberto.chajin@ciberdix.com>
2017-04-24 08:56:22 -0500

    Servicios de RolesFuncionalidades y RolesFuncionalidadesControles
    Servicios de RolesFuncionalidades y RolesFuncionalidadesControles
    apuntando a las vistas

--- Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
+++ Dominio/src/main/java/com/ciberdix/th/configuration/RepositoryConfig.java
@@ -173,5 +173,7 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
         config.exposeIdsFor(VFuncionalidades.class);
         config.exposeIdsFor(VFuncionalidadesControles.class);
         config.exposeIdsFor(VUsuarios.class);
+        config.exposeIdsFor(VRolesFuncionalidades.class);
+        config.exposeIdsFor(VRolesFuncionalidadesControles.class);
     }
 }
--- Negocio/src/main/java/com/ciberdix/th/controllers/refactor/RolesFuncionalidadesControlesRefactorController.java
+++ Negocio/src/main/java/com/ciberdix/th/controllers/refactor/RolesFuncionalidadesControlesRefactorController.java
@@ -1,6 +1,7 @@
 package com.ciberdix.th.controllers.refactor;
 
 import com.ciberdix.th.model.refactor.RolesFuncionalidadesControles;
+import com.ciberdix.th.model.refactor.VRolesFuncionalidadesControles;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -19,26 +20,26 @@ public class RolesFuncionalidadesControlesRefactorController {
     private String baseUrl;
 
     @RequestMapping(method = RequestMethod.GET)
-    List<RolesFuncionalidadesControles> findAll() {
+    List<VRolesFuncionalidadesControles> findAll() {
         String serviceUrl = baseUrl + "/api/rolesFuncionalidadesControles";
         RestTemplate restTemplate = new RestTemplate();
-        RolesFuncionalidadesControles[] parametros = restTemplate.getForObject(serviceUrl, RolesFuncionalidadesControles[].class);
+        VRolesFuncionalidadesControles[] parametros = restTemplate.getForObject(serviceUrl, VRolesFuncionalidadesControles[].class);
         return Arrays.asList(parametros);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/enabled")
-    List<RolesFuncionalidadesControles> findEnabled() {
+    List<VRolesFuncionalidadesControles> findEnabled() {
         String serviceUrl = baseUrl + "/api/rolesFuncionalidadesControles/";
         RestTemplate restTemplate = new RestTemplate();
-        RolesFuncionalidadesControles[] parametros = restTemplate.getForObject(serviceUrl + "enabled/", RolesFuncionalidadesControles[].class);
+        VRolesFuncionalidadesControles[] parametros = restTemplate.getForObject(serviceUrl + "enabled/", VRolesFuncionalidadesControles[].class);
         return Arrays.asList(parametros);
     }
 
     @RequestMapping(method = RequestMethod.GET, path="/{id}")
-    RolesFuncionalidadesControles findOne(@PathVariable Integer id) {
+    VRolesFuncionalidadesControles findOne(@PathVariable Integer id) {
         String serviceUrl = baseUrl + "/api/rolesFuncionalidadesControles/";
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(serviceUrl + id, RolesFuncionalidadesControles.class);
+        return restTemplate.getForObject(serviceUrl + id, VRolesFuncionalidadesControles.class);
     }
 
     @RequestMapping(method = RequestMethod.POST)
