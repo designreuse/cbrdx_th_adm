e527bdccec573a8a875b519ae1f67f766821c0d3
hackup86 <felipe.aguirre86@hotmail.com>
2017-04-24 11:09:39 -0500

    Correcciones y Creacion
    Objetos para ListasClasificaciones

--- Dominio/src/main/java/com/ciberdix/th/controllers/refactor/ListasClasificacionesRefactorController.java
+++ Dominio/src/main/java/com/ciberdix/th/controllers/refactor/ListasClasificacionesRefactorController.java
@@ -1,7 +1,7 @@
 package com.ciberdix.th.controllers.refactor;
 
-import com.ciberdix.th.models.refactor.ListasFactoresRh;
-import com.ciberdix.th.repositories.refactor.ListasFactorRhRefactorRepository;
+import com.ciberdix.th.models.refactor.ListasClasificaciones;
+import com.ciberdix.th.repositories.refactor.ListasClasificacionesRefactorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -9,54 +9,53 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * @author Roberto Chajin Ortiz
+ * Created by robertochajin on 18/04/17.
  */
-@RestController
 @CrossOrigin
 @Transactional
-@RequestMapping("/api/ListasFactoresRH")
-public class ListasFactoresRhRefactorController {
-
+@RestController
+@RequestMapping("/api/ListasClasificaciones")
+public class ListasClasificacionesRefactorController {
     @Autowired
-    private ListasFactorRhRefactorRepository repository;
+    private ListasClasificacionesRefactorRepository repository;
 
     @RequestMapping(method = RequestMethod.GET)
-    List<ListasFactoresRh> findAll() {
-        return (List<ListasFactoresRh>) repository.findAll();
+    List<ListasClasificaciones> findAll() {
+        return (List<ListasClasificaciones>) repository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/enabled/")
-    List<ListasFactoresRh> findEnabled() {
+    List<ListasClasificaciones> findEnabled() {
         return repository.findByIndicadorHabilitadoTrue();
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/code/{queryString}/")
-    ListasFactoresRh findByCode(@PathVariable String queryString) {
+    ListasClasificaciones findByCode(@PathVariable String queryString) {
         return repository.findByIndicadorHabilitadoTrueAndCodigo(queryString);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/codeStarts/{queryString}/")
-    List<ListasFactoresRh> findByCodeStarts(@PathVariable String queryString) {
+    List<ListasClasificaciones> findByCodeStarts(@PathVariable String queryString) {
         return repository.findByIndicadorHabilitadoTrueAndCodigoStartsWith(queryString);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/name/{queryString}/")
-    List<ListasFactoresRh> findByName(@PathVariable String queryString) {
+    List<ListasClasificaciones> findByName(@PathVariable String queryString) {
         return repository.findByIndicadorHabilitadoTrueAndNombreContains(queryString);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
-    ListasFactoresRh findOne(@PathVariable Integer id) {
+    ListasClasificaciones findOne(@PathVariable Integer id) {
         return repository.findOne(id);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    ListasFactoresRh create(@RequestBody ListasFactoresRh obj) {
-        return repository.save(new ListasFactoresRh(obj.getCodigo(), obj.getNombre(), obj.getOrden(), obj.getIndicadorHabilitado(), obj.getAuditoriaUsuario()));
+    ListasClasificaciones create(@RequestBody ListasClasificaciones obj) {
+        return repository.save(new ListasClasificaciones(obj.getCodigo(), obj.getNombre(), obj.getOrden(), obj.getIndicadorHabilitado(), obj.getAuditoriaUsuario()));
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    ListasFactoresRh update(@RequestBody ListasFactoresRh obj) {
+    ListasClasificaciones update(@RequestBody ListasClasificaciones obj) {
         return repository.save(obj);
     }
 }
