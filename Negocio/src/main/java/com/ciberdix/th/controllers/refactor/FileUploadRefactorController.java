20dfc7c548a24d8eee9612cc37564ef60ed05726
robertochajin <roberto.chajin@ciberdix.com>
2017-05-08 11:23:19 -0500

    Servicio de Carga de archivos
    Servicio de Carga de archivos

--- Negocio/src/main/java/com/ciberdix/th/controllers/refactor/FileUploadRefactorController.java
+++ Negocio/src/main/java/com/ciberdix/th/controllers/refactor/FileUploadRefactorController.java
@@ -1,15 +1,25 @@
 package com.ciberdix.th.controllers.refactor;
 
+import com.ciberdix.th.model.refactor.Terceros;
+import com.ciberdix.th.model.refactor.Usuarios;
+import com.ciberdix.th.security.JwtTokenUtil;
+import com.ciberdix.th.storage.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,66 +37,45 @@ import java.util.stream.Collectors;
 public class FileUploadRefactorController {
 
     private final Logger logger = LoggerFactory.getLogger(FileUploadRefactorController.class);
+    private final StorageService storageService;
 
-    private static String UPLOADED_FOLDER = "/Users/robertochajin/Ciberdix/";
+    @Value("${domain.url}")
+    private String serviceUrl;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    public FileUploadRefactorController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> uploadFile(
-            @RequestParam("file") MultipartFile uploadfile) {
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization");
 
-        logger.debug("Single file upload!");
+        int id = jwtTokenUtil.getIdUsernameFromToken(authToken);
+        RestTemplate restTemplate = new RestTemplate();
+        Usuarios usuario = restTemplate.getForObject(serviceUrl + "/api/usuarios/query/" + id, Usuarios.class);
+        Terceros tercero = restTemplate.getForObject(serviceUrl + "/api/terceros/" + usuario.getIdTercero(), Terceros.class);
+        //,RedirectAttributes redirectAttributes
+        String avatarGuardado = storageService.store(file);
+//        redirectAttributes.addFlashAttribute("message",
+//                "You successfully uploaded " + avatarGuardado + "!");
 
-        if (uploadfile.isEmpty()) {
-            return new ResponseEntity("please select a file!", HttpStatus.OK);
-        }
+        tercero.setImagen(avatarGuardado);
+        restTemplate.put(serviceUrl + "api/terceros", tercero);
 
-        try {
-
-            saveUploadedFiles(Arrays.asList(uploadfile));
-
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        return ResponseEntity.ok(UPLOADED_FOLDER + uploadfile.getOriginalFilename());
-        //return new ResponseEntity("Successfully uploaded - " + uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
-
+        return ResponseEntity.ok(avatarGuardado);
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/multiple")
-    ResponseEntity<?> uploadFileMulti(@RequestParam("files") MultipartFile[] uploadfiles) {
+    @RequestMapping(method = RequestMethod.GET, path = "/file/{filename}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-        logger.debug("Multiple file upload!");
-
-        // Get file name
-        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
-                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
-
-        if (StringUtils.isEmpty(uploadedFileName)) {
-            return new ResponseEntity("please select a file!", HttpStatus.OK);
-        }
-
-        try {
-            saveUploadedFiles(Arrays.asList(uploadfiles));
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity("Successfully uploaded - "
-                + uploadedFileName, HttpStatus.OK);
-
-    }
-
-    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
-        for (MultipartFile file : files) {
-
-            if (file.isEmpty()) {
-                continue; //next pls
-            }
-
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
-        }
+        Resource file = storageService.loadAsResource(filename + ".png");
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
     }
 }
