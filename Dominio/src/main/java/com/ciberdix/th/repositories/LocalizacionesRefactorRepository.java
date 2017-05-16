4147a07e1630145813ae77e8e694dca0abc18879
Darevalo95 <darevalo.djar95@gmail.com>
2017-05-16 11:09:49 -0500

    Se agrega Servicio
    Se agrega Localizacion por idTercero

--- Dominio/src/main/java/com/ciberdix/th/repositories/LocalizacionesRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/LocalizacionesRefactorRepository.java
@@ -1,10 +1,19 @@
 package com.ciberdix.th.repositories;
 
 import com.ciberdix.th.models.Localizaciones;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by Felipe Alejandro Aguirre Santos on 24/03/2017.
  */
 public interface LocalizacionesRefactorRepository extends CrudRepository<Localizaciones, Integer> {
+
+    @Query("SELECT l FROM Localizaciones l WHERE l.idLocalizacion IN (" +
+            "SELECT tl.idlocalizacion FROM TercerosLocalizaciones tl WHERE tl.idTercero = ?1 AND tl.idTerceroLocalizacion IN (" +
+            "SELECT tr.idTerceroLocalizacion FROM TercerosResidencias tr WHERE tr.indicadorHabilitado = 1))")
+    List<Localizaciones> queryByIdTercero(Long idTercero);
+
 }
