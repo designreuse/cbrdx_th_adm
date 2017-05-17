318723bc86dd46d1a899f8904cf06a0f3e24b650
Darevalo95 <darevalo.djar95@gmail.com>
2017-05-17 14:54:08 -0500

    Servicio
    Nuevo Servicio RequerimientosCompras

--- Dominio/src/main/java/com/ciberdix/th/models/RequerimientosCompras.java
+++ Dominio/src/main/java/com/ciberdix/th/models/RequerimientosCompras.java
@@ -1,15 +1,13 @@
 package com.ciberdix.th.models;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
  * Created by Felipe Alejandro Aguirre Santos on 24/03/2017.
  */
 @Entity
+@Table(name = "RequerimientosCompras", schema = "crz_th", catalog = "CREZCAMOS")
 public class RequerimientosCompras {
     private Integer idRequerimientoCompra;
     private Integer idRequerimiento;
@@ -17,7 +15,18 @@ public class RequerimientosCompras {
     private Integer auditoriaUsuario;
     private Timestamp auditoriaFecha;
 
+    public RequerimientosCompras() {
+    }
+
+    public RequerimientosCompras(Integer idRequerimiento, Integer idCompra, Integer auditoriaUsuario) {
+        this.idRequerimiento = idRequerimiento;
+        this.idCompra = idCompra;
+        this.auditoriaUsuario = auditoriaUsuario != null ? auditoriaUsuario : 1;
+        this.auditoriaFecha = new Timestamp(System.currentTimeMillis());
+    }
+
     @Id
+    @GeneratedValue
     @Column(name = "IdRequerimientoCompra")
     public Integer getIdRequerimientoCompra() {
         return idRequerimientoCompra;
