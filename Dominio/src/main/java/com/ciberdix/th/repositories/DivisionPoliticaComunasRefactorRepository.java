9b89d66cd08b4954e6b9b5905be3d30f8243c560
hackup86 <felipe.aguirre86@hotmail.com>
2017-05-23 13:24:06 -0500

    Remocion de Servicios de Division Politica
    Se remueven servicios adicionales de Division Politica

--- Dominio/src/main/java/com/ciberdix/th/repositories/DivisionPoliticaComunasRefactorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ciberdix.th.repositories;
-
-import com.ciberdix.th.models.DivisionPoliticaComunas;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DivisionPoliticaComunasRefactorRepository extends CrudRepository<DivisionPoliticaComunas, Integer> {
-}
