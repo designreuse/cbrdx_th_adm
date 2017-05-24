d8ae790975f89b1fef0a0b454616511af7078d4b
Felipe Alejandro Aguirre Santos <felipe.aguirre86@hotmail.com>
2017-05-24 13:55:20 -0500

    Correccion Servicio de Localizaciones
    Se corrige el servicios por cambios de estructura de DivisionPolitica

--- Dominio/src/main/java/com/ciberdix/th/repositories/VDivisionPoliticaRecRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/VDivisionPoliticaRecRefactorRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
  */
 public interface VDivisionPoliticaRecRefactorRepository extends CrudRepository<VDivisionPoliticaRec, Integer> {
 
-    @Query("SELECT u from VDivisionPoliticaRec u where u.camino like concat('%',?1,'%') and u.idDivisionPoliticaTipo in (select idDivisionPoliticaTipo from DivisionPoliticaTipos where length(codigoDivisionPoliticaTipo)>2)")
+    @Query("SELECT u from VDivisionPoliticaRec u where u.camino like concat('%',?1,'%') and u.idDivisionPoliticaTipo in (select idDivisionPoliticaTipo from DivisionPoliticaTipos where substring(codigoDivisionPoliticaTipo,1,1) IN  ('3','4'))")
     List<VDivisionPoliticaRec> queryLocations(String queryString);
 
-    @Query("SELECT u from VDivisionPoliticaRec u where u.camino like concat('%',?1,'%') and u.idDivisionPoliticaTipo in (select idDivisionPoliticaTipo from DivisionPoliticaTipos where length(codigoDivisionPoliticaTipo)=3)")
+    @Query("SELECT u from VDivisionPoliticaRec u where u.camino like concat('%',?1,'%') and u.idDivisionPoliticaTipo in (select idDivisionPoliticaTipo from DivisionPoliticaTipos where substring(codigoDivisionPoliticaTipo,1,1) IN  ('3'))")
     List<VDivisionPoliticaRec> queryAllCities(String queryString);
 
 }
