b88f7fe032838426714108508598167a60fddb1c
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-06-06 10:16:23 -0500

    Correo Electronico para los Referidos a los Procesos de Seleccion

--- Dominio/src/main/java/com/ciberdix/th/repositories/ProcesosRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/ProcesosRefactorRepository.java
@@ -1,10 +1,18 @@
 package com.ciberdix.th.repositories;
 
 import com.ciberdix.th.models.Procesos;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Danny on 5/06/2017.
  */
 public interface ProcesosRefactorRepository extends CrudRepository<Procesos, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Procesos SET Procesos.indicadorHabilitado = 0, Procesos.idEstado = ?1")
+    void disableRegisters(Integer idEstado);
 }
