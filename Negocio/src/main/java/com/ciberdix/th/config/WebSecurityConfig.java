7d92c9c357a1b981fb0d36ee0ab73231bbdd4943
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-06-09 10:35:56 -0500

    Refresh de token para redes sociales

--- Negocio/src/main/java/com/ciberdix/th/config/WebSecurityConfig.java
+++ Negocio/src/main/java/com/ciberdix/th/config/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers("/swagger-resources/**").permitAll()
                 //.antMatchers("/v2/**").permitAll()
                 .antMatchers("/api/requerimientos/externaMixta").permitAll()
+                .antMatchers("/api/constantes/codigos").permitAll()
                 .anyRequest().authenticated();
 
         httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
