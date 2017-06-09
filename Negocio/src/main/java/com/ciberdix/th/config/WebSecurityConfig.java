0387c42db1e9f50130b789e5d1e30042011c5cf7
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-06-09 10:33:12 -0500

    Refresh de token para redes sociales

--- Negocio/src/main/java/com/ciberdix/th/config/WebSecurityConfig.java
+++ Negocio/src/main/java/com/ciberdix/th/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import com.ciberdix.th.security.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -59,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers("/webjars/**").permitAll()
                 //.antMatchers("/swagger-resources/**").permitAll()
                 //.antMatchers("/v2/**").permitAll()
-                .antMatchers("/api/publicaciones/externaMixta").permitAll()
+                .antMatchers("/api/requerimientos/externaMixta").permitAll()
                 .anyRequest().authenticated();
 
         httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
