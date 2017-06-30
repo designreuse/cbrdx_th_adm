46d0907dcf9ab9dc08522b26d6bf565c255239f5
robertochajin <robertochajin@gmail.com>
2017-06-30 18:36:24 -0500

    cleaning code
    cleaning code

--- Negocio/src/main/java/com/ciberdix/th/controllers/ReglasFormulariosCargosRefactorController.java
+++ Negocio/src/main/java/com/ciberdix/th/controllers/ReglasFormulariosCargosRefactorController.java
@@ -69,79 +69,7 @@ public class ReglasFormulariosCargosRefactorController {
                 responseJson.put(p.getCodigo(), securityData);
             }
         }
-/*
-        for (VPermisosFormulariosCargos p : allPermisos) {
-            if (p.getIndicadorSeccion()) {
-//                HashMap<String, Object> hmap = new HashMap<>();
-                HashMap<String, Object> obj = new HashMap<>();
-                HashMap<String, Object> dataControl = new HashMap<>();
 
-                if (p.getIndicadorHabilitadoFc() != null && p.getIndicadorHabilitadoFc()
-                        && p.getIndicadorHabilitadoRfc() != null && p.getIndicadorHabilitadoRfc()) {
-                    obj.put("codigo", p.getCodigo());
-                    obj.put("visible", Boolean.TRUE);
-
-                    for (VPermisosFormulariosCargos d : allPermisos) {
-                        if (!d.getIndicadorSeccion()) {
-                            if (d.getIndicadorHabilitadoFc() != null && d.getIndicadorHabilitadoFc()
-                                    && d.getIndicadorHabilitadoRfc() != null && d.getIndicadorHabilitadoRfc()) {
-//                                dataControl.put("codigo", d.getCodigo());
-                                dataControl.put("visible", Boolean.TRUE);
-
-                                if (d.getIndicadorEditar()) {
-                                    dataControl.put("editable", Boolean.TRUE);
-                                } else {
-                                    dataControl.put("editable", Boolean.FALSE);
-                                }
-                            } else {
-                                dataControl.put("visible", Boolean.FALSE);
-
-                                if (d.getIndicadorEditar()) {
-                                    dataControl.put("editable", Boolean.TRUE);
-                                } else {
-                                    dataControl.put("editable", Boolean.FALSE);
-                                }
-                            }
-
-                            obj.put(d.getCodigo(), dataControl);
-                        }
-                    }
-
-                    //hmap.put("data", obj);
-                } else {
-                    obj.put("codigo", p.getCodigo());
-                    obj.put("visible", Boolean.FALSE);
-
-                    for (VPermisosFormulariosCargos d : allPermisos) {
-                        if (!d.getIndicadorSeccion()) {
-                            if (d.getIndicadorHabilitadoFc() != null && d.getIndicadorHabilitadoFc()
-                                    && d.getIndicadorHabilitadoRfc() != null && d.getIndicadorHabilitadoRfc()) {
-                                dataControl.put("visible", Boolean.TRUE);
-
-                                if (d.getIndicadorEditar()) {
-                                    dataControl.put("editable", Boolean.TRUE);
-                                } else {
-                                    dataControl.put("editable", Boolean.FALSE);
-                                }
-                            } else {
-                                dataControl.put("visible", Boolean.FALSE);
-
-                                if (d.getIndicadorEditar()) {
-                                    dataControl.put("editable", Boolean.TRUE);
-                                } else {
-                                    dataControl.put("editable", Boolean.FALSE);
-                                }
-                            }
-
-                            obj.put(d.getCodigo(), dataControl);
-                        }
-                    }
-                    //hmap.put(p.getCodigo(), obj);
-                }
-                permisosForm.add(obj);
-            }
-        }
-*/
         return objectMapper.valueToTree(responseJson);
     }
 }
