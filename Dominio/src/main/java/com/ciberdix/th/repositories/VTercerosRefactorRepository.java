73c4aa9dda9200500bd09af5da3def8d3c8163f0
Danny Arevalo <darevalo.djar95@gmail.com>
2017-07-04 17:25:25 -0500

    Arreglo en VTerceros
    Arreglo en VTerceros

--- Dominio/src/main/java/com/ciberdix/th/repositories/VTercerosRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/VTercerosRefactorRepository.java
@@ -5,6 +5,7 @@
  */
 package com.ciberdix.th.repositories;
 
+import com.ciberdix.th.models.VTerceros;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
