036357113b769890a2d664c8299b3db005e2e248
Danny Arevalo <darevalo.djar95@gmail.com>
2017-07-11 17:30:50 -0500

    Modifica el Servicio
    Se modifica el servicio de Examenes medicos, añadiendo una consulta por
    proceso seleccion.

--- Dominio/src/main/java/com/ciberdix/th/repositories/VExamenesMedicosRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/VExamenesMedicosRefactorRepository.java
@@ -3,8 +3,13 @@ package com.ciberdix.th.repositories;
 import com.ciberdix.th.models.VExamenesMedicos;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Danny on 11/07/2017.
  */
 public interface VExamenesMedicosRefactorRepository extends CrudRepository<VExamenesMedicos, Integer> {
+
+    List<VExamenesMedicos> findAllByIdProcesoSeleccion(Integer id);
+
 }
