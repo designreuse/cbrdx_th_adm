c14af458afa236059219960b0ca3d021a0cb5cf7
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-07-12 12:22:00 -0500

    Servicio de Formularios

--- Dominio/src/main/java/com/ciberdix/th/repositories/VPublicacionesCuestionariosRefactorRepository.java
+++ Dominio/src/main/java/com/ciberdix/th/repositories/VPublicacionesCuestionariosRefactorRepository.java
@@ -12,4 +12,6 @@ public interface VPublicacionesCuestionariosRefactorRepository extends CrudRepos
 
     List<VPublicacionesCuestionarios> findAllByIdPublicacion(Integer id);
 
+    List<VPublicacionesCuestionarios> findAllByIdPublicacionAAndIndicadorHabilitadoTrue(Integer id);
+
 }
