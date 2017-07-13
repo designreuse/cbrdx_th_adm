337dfb55757acc519104a22bb11bb6b5317be919
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-07-13 09:38:48 -0500

    Cambios de Unificacion de Envio de Correos

--- Negocio/src/main/java/com/ciberdix/th/controllers/ExamenesMedicosRefactorController.java
+++ Negocio/src/main/java/com/ciberdix/th/controllers/ExamenesMedicosRefactorController.java
@@ -1,13 +1,14 @@
 package com.ciberdix.th.controllers;
 
-import com.ciberdix.th.config.Globales;
 import com.ciberdix.th.model.ExamenesMedicos;
 import com.ciberdix.th.model.ProcesoSeleccion;
+import com.ciberdix.th.model.TercerosPublicaciones;
 import com.ciberdix.th.model.VExamenesMedicos;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,41 +21,51 @@ import java.util.List;
 @RequestMapping("/api/examenesMedicos")
 public class ExamenesMedicosRefactorController {
 
-    @Value("${business.url}")
-    String businessURL;
+    @Value("${domain.url}")
+    private String baseUrl;
 
-    Globales globales = new Globales();
-    private String serviceUrl = globales.getUrl() + "/api/examenesMedicos";
+    @Value("${business.url}")
+    private String businessUrl;
+
+    private String serviceUrl;
+
+    private RestTemplate restTemplate;
+
+    @PostConstruct
+    void init() {
+        serviceUrl = baseUrl + "/api/examenesMedicos/";
+        restTemplate = new RestTemplate();
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     List<VExamenesMedicos> findAll() {
-        RestTemplate restTemplate = new RestTemplate();
         VExamenesMedicos[] parametros = restTemplate.getForObject(serviceUrl, VExamenesMedicos[].class);
         return Arrays.asList(parametros);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     VExamenesMedicos findOne(@PathVariable Integer id) {
-        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(serviceUrl + "/" + id, VExamenesMedicos.class);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/procesoSeleccion/{id}")
     VExamenesMedicos getByIdProcesoSeleccion(@PathVariable Integer id) {
-        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(serviceUrl + "/procesoSeleccion/" + id, VExamenesMedicos.class);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     ExamenesMedicos create(@RequestBody ExamenesMedicos obj) {
-        RestTemplate restTemplate = new RestTemplate();
-        ProcesoSeleccion p = restTemplate.getForObject(globales.getUrl() + "/api/procesoSeleccion/" + obj.getIdProcesoSeleccion(), ProcesoSeleccion.class);
+        ProcesoSeleccion p = restTemplate.getForObject(baseUrl + "/api/procesoSeleccion/" + obj.getIdProcesoSeleccion(), ProcesoSeleccion.class);
+        TercerosPublicaciones tercerosPublicaciones = restTemplate.getForObject(baseUrl + "/api/tercerosPublicaciones/" + p.getIdTerceroPublicacion(), TercerosPublicaciones.class);
         String tokenProfile = UtilitiesController.generateTokenButton("/profile", null);
         if (obj.getIdInstitucionMedica() != null) {
             String token810 = UtilitiesController.generateTokenButton("/answer-exams/exam/" + obj.getIdExamenMedico() + "/terceroPublicacion/" + p.getIdTerceroPublicacion(), null);
             String token811 = UtilitiesController.generateTokenButton("/informed-consent/exam/" + obj.getIdExamenMedico() + "/terceroPublicacion/" + p.getIdTerceroPublicacion(), null);
+            UtilitiesController.sendMail("w1andresv@gmail.com", "810", "respondercuest" + token810 + "perfil" + tokenProfile);
+            UtilitiesController.sendMail("w1andresv@gmail.com", "811", "consentimiento" + token811);
         } else {
             String token814 = UtilitiesController.generateTokenButton("/informed-consent/exam/" + obj.getIdExamenMedico() + "/terceroPublicacion/" + p.getIdTerceroPublicacion(), null);
+            UtilitiesController.sendMail("w1andresv@gmail.com", "810", "consentimiento" + token814 + "perfil" + tokenProfile);
         }
         //Correo 810 y 811 si InstMedic!=null o 814 si null
         // url 810 es /answer-exams/exam/:idExamen/terceroPublicacion/:idTerceroPublication
@@ -65,7 +76,6 @@ public class ExamenesMedicosRefactorController {
 
     @RequestMapping(method = RequestMethod.PUT)
     void update(@RequestBody ExamenesMedicos obj) {
-        RestTemplate restTemplate = new RestTemplate();
         //Correo 810 y 811 si Estado = ENESPR e InstMedic!=null
         restTemplate.put(serviceUrl, obj);
     }
