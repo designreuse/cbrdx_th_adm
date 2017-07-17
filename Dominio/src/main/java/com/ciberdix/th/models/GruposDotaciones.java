230732e94965c935226754a6767186e67bf8c1b7
Danny Arevalo <darevalo.djar95@gmail.com>
2017-07-17 10:28:11 -0500

    Nuevo Servicio
    Se agrega el servicio de GruposDotaciones

--- Dominio/src/main/java/com/ciberdix/th/models/GruposDotaciones.java
+++ Dominio/src/main/java/com/ciberdix/th/models/GruposDotaciones.java
@@ -1,15 +1,13 @@
 package com.ciberdix.th.models;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
- * Created by Felipe Alejandro Aguirre Santos on 24/03/2017.
+ * Created by Danny on 17/07/2017.
  */
 @Entity
+@Table(name = "GruposDotaciones", schema = "dbo", catalog = "CREZCAMOS")
 public class GruposDotaciones {
     private Integer idGrupoDotacion;
     private String grupoDotacion;
@@ -17,8 +15,32 @@ public class GruposDotaciones {
     private Boolean indicadorHabilitado;
     private Integer auditoriaUsuario;
     private Timestamp auditoriaFecha;
+    private String codigo;
+
+    public GruposDotaciones() {
+    }
+
+    public GruposDotaciones(String grupoDotacion, String descripcion, Boolean indicadorHabilitado, Integer auditoriaUsuario, String codigo) {
+        this.grupoDotacion = grupoDotacion;
+        this.descripcion = descripcion;
+        this.indicadorHabilitado = indicadorHabilitado;
+        this.auditoriaUsuario = auditoriaUsuario != null ? auditoriaUsuario : 1;
+        this.auditoriaFecha = new Timestamp(System.currentTimeMillis());
+        this.codigo = codigo;
+    }
+
+    public GruposDotaciones(Integer idGrupoDotacion, String grupoDotacion, String descripcion, Boolean indicadorHabilitado, Integer auditoriaUsuario, String codigo) {
+        this.idGrupoDotacion = idGrupoDotacion;
+        this.grupoDotacion = grupoDotacion;
+        this.descripcion = descripcion;
+        this.indicadorHabilitado = indicadorHabilitado;
+        this.auditoriaUsuario = auditoriaUsuario != null ? auditoriaUsuario : 1;
+        this.auditoriaFecha = new Timestamp(System.currentTimeMillis());
+        this.codigo = codigo;
+    }
 
     @Id
+    @GeneratedValue
     @Column(name = "IdGrupoDotacion", nullable = false)
     public Integer getIdGrupoDotacion() {
         return idGrupoDotacion;
@@ -78,6 +100,16 @@ public class GruposDotaciones {
         this.auditoriaFecha = auditoriaFecha;
     }
 
+    @Basic
+    @Column(name = "Codigo", nullable = true, length = 10)
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -96,6 +128,7 @@ public class GruposDotaciones {
             return false;
         if (auditoriaFecha != null ? !auditoriaFecha.equals(that.auditoriaFecha) : that.auditoriaFecha != null)
             return false;
+        if (codigo != null ? !codigo.equals(that.codigo) : that.codigo != null) return false;
 
         return true;
     }
@@ -108,6 +141,7 @@ public class GruposDotaciones {
         result = 31 * result + (indicadorHabilitado != null ? indicadorHabilitado.hashCode() : 0);
         result = 31 * result + (auditoriaUsuario != null ? auditoriaUsuario.hashCode() : 0);
         result = 31 * result + (auditoriaFecha != null ? auditoriaFecha.hashCode() : 0);
+        result = 31 * result + (codigo != null ? codigo.hashCode() : 0);
         return result;
     }
 }
