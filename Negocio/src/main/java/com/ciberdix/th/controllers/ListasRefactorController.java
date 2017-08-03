b0c8a95620f3d19fa9e5658925758d841c9f1ce9
Felipe Aguirre <felipe.aguirre@ciberdix.com>
2017-08-03 12:39:04 -0500

    Cambios para Incluir RelacionesListas en TH

--- Negocio/src/main/java/com/ciberdix/th/controllers/ListasRefactorController.java
+++ Negocio/src/main/java/com/ciberdix/th/controllers/ListasRefactorController.java
@@ -1,14 +1,15 @@
 package com.ciberdix.th.controllers;
 
 import com.ciberdix.th.config.Globales;
-import com.ciberdix.th.model.Listas;
-import com.ciberdix.th.model.ListasItems;
+import com.ciberdix.th.model.*;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Roberto Chajin Ortiz
@@ -28,10 +29,57 @@ public class ListasRefactorController {
         return Arrays.asList(generos);
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/buscarRelacion/{code}")
+    List<ListasItems> findList(@PathVariable String code) {
+        RestTemplate restTemplate = new RestTemplate();
+        RelacionesListas relacion = restTemplate.getForObject(globales.getUrl() + "/api/relacionesListas/code/" + code, RelacionesListas.class);
+        Integer idLista = relacion.getIdListaHijo();
+        Listas listaHija = restTemplate.getForObject(serviceUrl + "listas/buscarId/" + idLista, Listas.class);
+        RelacionesListasHijos[] hijos = restTemplate.getForObject(globales.getUrl() + "/api/relacionesListasHijos/relacionesListas/" + relacion.getIdRelacionLista() + "/enabled", RelacionesListasHijos[].class);
+        ListasItems[] parameter = restTemplate.getForObject(serviceUrl + "/" + listaHija.getNombreTabla() + "/enabled/", ListasItems[].class);
+        List<ListasItems> resultado = new ArrayList<>();
+        for (RelacionesListasHijos rlh : hijos) {
+            Integer id = rlh.getIdItemHijo();
+            for (ListasItems li : parameter) {
+                if (li.getIdLista().equals(id)) {
+                    resultado.add(li);
+                    break;
+                }
+            }
+        }
+        return resultado;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/buscarRelacionFuncional/{padre}/{idItem}/{control}")
+    List<ListasItems> findFromRelationship(@PathVariable String padre, @PathVariable Integer idItem, @PathVariable String control) {
+        List<ListasItems> resultado = new ArrayList<>();
+        RestTemplate restTemplate = new RestTemplate();
+        Listas listaPadre = restTemplate.getForObject(serviceUrl + "listas/buscarTabla/" + padre, Listas.class);
+        FuncionalidadesControles controlHijo = restTemplate.getForObject(serviceUrl + "funcionalidadesControles/codigo/" + control, FuncionalidadesControles.class);
+        List<RelacionesListas> padres = Arrays.asList(restTemplate.getForObject(serviceUrl + "relacionesListas/findByParents/" + listaPadre.getIdLista() + "/" + idItem, RelacionesListas[].class));
+        List<RelacionesListasCampos> hijos = Arrays.asList(restTemplate.getForObject(serviceUrl + "relacionesListasCampos/control/" + controlHijo.getIdFuncionalidadControl(), RelacionesListasCampos[].class));
+        for (RelacionesListasCampos hijo : hijos) {
+            List<RelacionesListas> test = padres.stream().filter(a -> a.getIdRelacionLista().equals(hijo.getIdRelacionLista())).collect(Collectors.toList());
+            if (!test.isEmpty()) {
+                return findList(test.get(0).getCodigo());
+            }
+        }
+        return resultado;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/buscarId/{idLista}")
     Listas findOne(@PathVariable Integer idLista) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(serviceUrl + "listas/buscarId/" + idLista, Listas.class);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/tablas/{idTable}")
+    List<ListasItems> findAllById(@PathVariable Integer idTable) {
+        RestTemplate restTemplate = new RestTemplate();
+        Listas listas = restTemplate.getForObject(serviceUrl + "listas/buscarId/" + idTable, Listas.class);
+        String nombre = listas.getNombreTabla();
+        ListasItems[] parameter = restTemplate.getForObject(serviceUrl + "/" + nombre + "/enabled/", ListasItems[].class);
+        return Arrays.asList(parameter);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/tabla/{tableName}/")
